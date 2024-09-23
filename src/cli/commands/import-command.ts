48dd48ab6a7705191f6bf04aaaf704f3564a1837
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-09-23 17:27:31 +0700

    чтение большого файла

--- src/cli/commands/import-command.ts
+++ src/cli/commands/import-command.ts
@@ -1,9 +1,18 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '#shared/libs/file-reader/index.js';
 import { getErrorMessage } from '#shared/helpers/index.js';
+import { Offer } from '#shared/types/index.js';
 import { CommandType } from './const.js';
 
 export class ImportCommand implements Command {
+  private onImportedOffer(offer: Offer): void {
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
   public getName(): string {
     return CommandType.Import;
   }
@@ -12,9 +21,11 @@ export class ImportCommand implements Command {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedOffer);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
     } catch (error: unknown) {
       console.error(`Can't import data from file: ${filename}`);
       getErrorMessage(error);
