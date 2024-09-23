7441bb875e24ebaaa30eeef01306f917f93c7dcf
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-09-23 14:01:30 +0700

    вопросы

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@
 /*
 Вопросы
   1. краткий вызов getRandomItems(...) работает зачем дополнтильно указывать тип string?  getRandomItems<string>(...)
+
+Сделать
+  + 1. из TSVFileReader выделить OfferParser
+
 */
