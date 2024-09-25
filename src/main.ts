a269543ffd90b051e166789bc85536a065dfd9ae
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-09-25 11:19:00 +0700

    вопросы

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@
 
 /*
 Вопросы
-  1. краткий вызов getRandomItems(...) работает зачем дополнтильно указывать тип string?  getRandomItems<string>(...)
-  1. а почему OFFER_TYPES не string[] и приходиться [...OFFER_TYPES]
+  +1. краткий вызов getRandomItems(...) работает зачем дополнтильно указывать тип string?  getRandomItems<string>(...)
+  2. а почему OFFER_TYPES не string[] и приходиться [...OFFER_TYPES]
     т.к. export const OFFER_TYPES = ['apartment', 'house', 'room', 'hotel'] as const;  и это не string[], а readonly ['..','..']
 
 Сделать
