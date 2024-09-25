7563bbc6713bbb449fea5d8257c97d2c55114d8d
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-09-25 09:39:45 +0700

    лишнее

--- src/shared/libs/offer-generator/tsv-offer-generator.ts
+++ src/shared/libs/offer-generator/tsv-offer-generator.ts
@@ -16,24 +16,24 @@ export class TSVOfferGenerator implements OfferGenerator {
     const cityName = getRandomItem<CityName>(Object.values(CityName));
     const { location } = CityLocation[cityName];
 
-    const title = getRandomItem<string>(mockData.titles);
-    const description = getRandomItem<string>(mockData.descriptions);
+    const title = getRandomItem(mockData.titles);
+    const description = getRandomItem(mockData.descriptions);
     const publishDate = getRandomDate(mockData.publishDate.min, mockData.publishDate.max).toISOString();
     const city = cityName;
-    const previewImage = getRandomItem<string>(mockData.previewImages);
-    const images = getRandomItems<string>(mockData.images, IMAGES_COUNT, IMAGES_COUNT).join(';');
+    const previewImage = getRandomItem(mockData.previewImages);
+    const images = getRandomItems(mockData.images, IMAGES_COUNT, IMAGES_COUNT).join(';');
     const isPremium = getRandomBoolean();
     const isFavorite = getRandomBoolean();
     const rating = this.getRandomNumber(mockData.rating);
-    const type = getRandomItem<string>([...OFFER_TYPES]);
+    const type = getRandomItem([...OFFER_TYPES]);
     const rooms = this.getRandomNumber(mockData.rooms);
     const maxAdults = this.getRandomNumber(mockData.maxAdults);
     const price = this.getRandomNumber(mockData.price);
-    const goods = getRandomItems<string>([...OFFER_GOODS], getRandomNumber(0, OFFER_GOODS.length), 0).join(';');
-    const name = getRandomItem<string>(mockData.user.names);
-    const email = getRandomItem<string>(mockData.user.emails);
-    const avatarPath = getRandomItem<string>(mockData.user.avatarPaths);
-    const userType = getRandomItem<string>(Object.values(UserType));
+    const goods = getRandomItems([...OFFER_GOODS], getRandomNumber(0, OFFER_GOODS.length), 0).join(';');
+    const name = getRandomItem(mockData.user.names);
+    const email = getRandomItem(mockData.user.emails);
+    const avatarPath = getRandomItem(mockData.user.avatarPaths);
+    const userType = getRandomItem(Object.values(UserType));
     const latitude = round(location.latitude + this.getRandomNumber(deltaCityLocation.latitude), deltaCityLocation.latitude.numAfterDigit);
     const longitude = round(location.longitude + this.getRandomNumber(deltaCityLocation.longitude), deltaCityLocation.latitude.numAfterDigit);
 
