8e82cfa9a0a5435f2667ea95a560040be2be7ee6
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-09-25 11:18:10 +0700

    вопросы

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+
+/*
+Вопросы
+  +1. краткий вызов getRandomItems(...) работает зачем дополнтильно указывать тип string?  getRandomItems<string>(...)
+  2. а почему OFFER_TYPES не string[] и приходиться [...OFFER_TYPES]
+    т.к. export const OFFER_TYPES = ['apartment', 'house', 'room', 'hotel'] as const;  и это не string[], а readonly ['..','..']
+
+Сделать
+  + 1. из TSVFileReader выделить OfferParser
+
+*/
