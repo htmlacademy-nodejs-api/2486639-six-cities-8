ab52e8ddddc6ca0ea69123609e65068e48fe3b92
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-10-10 15:54:56 +0700

    Регистрация нового компонента DatabaseClient. Подключение к БД при старте приложения

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,20 +1,39 @@
 import { inject, injectable } from 'inversify';
+import { Component } from '../shared/types/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
-import { Component } from '../shared/types/index.js';
+import { DatabaseClient } from '../shared/libs/database-client/index.js';
+import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
+  private async initDadabase() {
+    const mongoURI = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoURI);
+  }
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient
   ) { }
 
   public async init() {
     this.logger.info('Application initialization');
-    //! временно
+    //! временно?
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
     this.logger.info(`Get value from env $DB_HOST: ${this.config.get('DB_HOST')}`);
     this.logger.info(`Get value from env $PORT: ${this.config.get('SALT')}`);
+
+    this.logger.info('Init database...');
+    await this.initDadabase();
+    this.logger.info('Init database completed');
   }
 }
