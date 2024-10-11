7d087cccdd26c3f26e7946cb5281be7a932ae862
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-11 14:32:25 +0700

    добавил компоненты

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -2,5 +2,7 @@ export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
-  DatabaseClient: Symbol.for('DatabaseClient')
+  DatabaseClient: Symbol.for('DatabaseClient'),
+  UserModel: Symbol.for('UserModel'),
+  UserService: Symbol.for('UserService')
 } as const;
