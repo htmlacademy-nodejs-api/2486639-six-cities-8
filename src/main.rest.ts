77fb23163abb8bf2f8fc112100ac4311073a0e78
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-11 14:44:10 +0700

    объеденил inversify Containers

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,24 +1,17 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { types } from '@typegoose/typegoose';
 import { Component } from './shared/types/index.js';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
-import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
-import { DefaultUserService, UserEntity, UserModel, UserService } from './shared/modules/user/index.js';
+import { createRestApplicationContainer, RestApplication } from './rest/index.js';
+import { createUserContainer } from './shared/modules/user/index.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
-  container.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
-  container.bind<UserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer()
+  );
 
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
