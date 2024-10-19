cf8127f24061cb80b73c9dbfed598030caba593b
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-10-19 21:56:54 +0700

    ошибка

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,7 +6,7 @@ export { randomNumberOption, MockServerData } from './mock-server-data.type.js';
 export { OFFER_TYPES, OfferType } from './offer-type.type.js';
 export { OFFER_GOODS, OfferGood, OfferGoods } from './offer-goods.type.js';
 export { OfferId, Offer, Offers } from './offer.type.js';
-export { ReviewId, Review, Reviews } from './review.type.js';
+export { Review, Reviews } from './review.type.js';
 export { Token, UserData } from './user-data.type.js';
 export { UserType } from './user-type.enum.js';
 export { User } from './user.type.js';
