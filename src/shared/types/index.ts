ac95e1ced41697e3fd3c899ffb1a35df9f5c91f0
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-21 12:00:41 +0700

    перечисление для сортировки

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,6 +7,7 @@ export { OFFER_TYPES, OfferType } from './offer-type.type.js';
 export { OFFER_GOODS, OfferGood, OfferGoods } from './offer-goods.type.js';
 export { OfferId, Offer, Offers } from './offer.type.js';
 export { Review, Reviews } from './review.type.js';
+export { SortType } from './sort-type.enum.js';
 export { Token, UserData } from './user-data.type.js';
 export { UserType } from './user-type.enum.js';
 export { User } from './user.type.js';
