f34bdbb8ff6fe0f03e59fa2bb1831332abeeaea5
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-22 11:19:56 +0700

    обертка для express

---
+++ src/shared/libs/rest/index.ts
@@ -0,0 +1,9 @@
+export { BaseController } from './controller/base-controller.abstract.js';
+export { Controller } from './controller/controller.interface.js';
+export { HttpError } from './errors/http-error.js';
+export { AppExceptionFilter } from './exception-filter/app-exception-filter.js';
+export { ExceptionFilter } from './exception-filter/exception-filter.interface.js';
+export { HttpMethod } from './types/http-method.enum.js';
+export { RequestBody } from './types/request-body.type.js';
+export { RequestParams } from './types/request.params.type.js';
+export { Route } from './types/route.interface.js';
