0fe8357584047d80462e4c00b8ce702a661f77b1
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-24 15:53:16 +0700

    подключил проверку id для предложений

--- src/shared/modules/offer/offer.const.ts
+++ src/shared/modules/offer/offer.const.ts
@@ -1,2 +1,8 @@
 export const DEFAULT_OFFER_COUNT = 60;
 export const PREMIUN_OFFER_COUNT = 3;
+export const OFFER_ID = 'offerId';
+export const OFFER_ID_PARAMETR = `:${OFFER_ID}`;
+export enum OfferRoute {
+  Root = '/',
+  OfferId = `/${OFFER_ID_PARAMETR}`
+}
--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { BaseController, HttpMethod } from '../../libs/rest/index.js';
+import { BaseController, HttpMethod, ValidateObjectIdMiddleware } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Component } from '../../types/index.js';
 import { CreateOfferRequest } from './create-offer-request.type.js';
@@ -9,6 +9,7 @@ import { OfferService } from './offer-service.interface.js';
 import { fillDTO } from '../../helpers/index.js';
 import { DetailOfferRdo } from './rdo/detail-offer.rdo.js';
 import { OfferRdo } from './rdo/offer.rdo.js';
+import { OFFER_ID, OfferRoute } from './offer.const.js';
 
 @injectable()
 export class OfferController extends BaseController {
@@ -18,11 +19,15 @@ export class OfferController extends BaseController {
   ) {
     super(logger);
 
-    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
-    this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/:offerId', method: HttpMethod.Patch, handler: this.update });
-    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
-    this.addRoute({ path: '/:offerId', method: HttpMethod.Delete, handler: this.delete });
+    const validateObjectIdMiddleware = new ValidateObjectIdMiddleware(OFFER_ID);
+    const middlewares = [validateObjectIdMiddleware];
+    //new DocumentExistsMiddleware(this.offerService, 'Offer'??, 'OFFER_ID');
+
+    this.addRoute({ path: OfferRoute.Root, method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: OfferRoute.Root, method: HttpMethod.Get, handler: this.index });
+    this.addRoute({ path: OfferRoute.OfferId, method: HttpMethod.Patch, handler: this.update, middlewares });
+    this.addRoute({ path: OfferRoute.OfferId, method: HttpMethod.Get, handler: this.show, middlewares });
+    this.addRoute({ path: OfferRoute.OfferId, method: HttpMethod.Delete, handler: this.delete, middlewares });
   }
 
   public async create({ body }: CreateOfferRequest, res: Response): Promise<void> {
@@ -69,15 +74,15 @@ export class OfferController extends BaseController {
   }
 
   public async show(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
+    const { offerId } = req.params;
     //! throw - "Cast to ObjectId failed for value \"67189abb70d1c82e25abc7b6-\" (type string) at path \"_id\" for model \"OfferEntity\""
     // null не возвращает...
-    const offer = await this.offerService.findById(id);
+    const offer = await this.offerService.findById(offerId);
 
     if (offer) {
       this.ok(res, fillDTO(DetailOfferRdo, offer));
     } else {
-      this.notFound(res, { id });
+      this.notFound(res, { offerId });
     }
   }
 
