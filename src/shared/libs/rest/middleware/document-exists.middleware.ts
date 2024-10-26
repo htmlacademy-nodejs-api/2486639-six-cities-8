d4f05ba9c8b90ed5fddc8653b6cce46098898631
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-10-26 16:59:07 +0700

    откат BaseMiddleware

--- src/shared/libs/rest/middleware/document-exists.middleware.ts
+++ src/shared/libs/rest/middleware/document-exists.middleware.ts
@@ -1,21 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { BaseMiddleware } from './base-middleware.abstract.js';
+import { Middleware } from './middleware.interface.js';
 import { DocumentExists } from '../../../types/index.js';
+import { HttpError } from '../errors/http-error.js';
 
-export class DocumentExistsMiddleware extends BaseMiddleware {
+export class DocumentExistsMiddleware implements Middleware {
   constructor(
     private readonly service: DocumentExists,
     private readonly entityName: string,
     private readonly paramName: string,
-  ) {
-    super();
-  }
+  ) { }
 
   public async execute({ params }: Request, _res: Response, next: NextFunction): Promise<void> {
     const documentId = params[this.paramName];
     if (! await this.service.exists(documentId)) {
-      this.throwHttpError(StatusCodes.NOT_FOUND, `${this.entityName} with ${documentId} not found.`);
+      throw new HttpError(StatusCodes.NOT_FOUND, `${this.entityName} with ${documentId} not found.`, this.constructor.name);
     }
 
     next();
--- src/shared/libs/rest/middleware/validate-objectid.middleware.ts
+++ src/shared/libs/rest/middleware/validate-objectid.middleware.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { Types } from 'mongoose';
 import { StatusCodes } from 'http-status-codes';
-import { BaseMiddleware } from './base-middleware.abstract.js';
+import { Middleware } from './middleware.interface.js';
+import { HttpError } from '../errors/http-error.js';
 
-export class ValidateObjectIdMiddleware extends BaseMiddleware {
+export class ValidateObjectIdMiddleware implements Middleware {
   constructor(private param: string) {
-    super();
   }
 
   public execute({ params }: Request, _res: Response, next: NextFunction): void {
@@ -15,6 +15,6 @@ export class ValidateObjectIdMiddleware extends BaseMiddleware {
       return next();
     }
 
-    this.throwHttpError(StatusCodes.BAD_REQUEST, `${objectId} is invalid ObjectID`);
+    throw new HttpError(StatusCodes.BAD_REQUEST, `${objectId} is invalid ObjectID`, this.constructor.name);
   }
 }
