7b42eb94d485590fbc58190093ea0e65018709a0
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-28 11:01:34 +0700

    настройки для PathTransformer

--- src/shared/libs/rest/transform/path-transformer.constant.ts
+++ src/shared/libs/rest/transform/path-transformer.constant.ts
@@ -1,9 +1,16 @@
 export const DEFAULT_STATIC_IMAGES = [
-  'default-avatar.svg'
+  'default-avatar.svg',
+  'avatar-angelina.jpg',
+  'avatar-max.jpg',
+  'apartment-01.jpg',
+  'apartment-02.jpg',
+  'apartment-03.jpg',
+  'room-01.jpg',
+  'studio-01.jpg'
 ];
 
 export const STATIC_RESOURCE_FIELDS = [
   'avatarPath',
-  'images',
-  'image'
+  'previewImage',
+  'images'
 ];
