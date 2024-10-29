f4ee55cbeadc669de9f104a5fdfb056279e3c423
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-29 17:56:08 +0700

    заметки

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -101,6 +101,7 @@ bootstrap();
   36. Проверить создание папки upload при первой загрузке изображения!
   37. что еще придумать! getRandomStringEnumValue и убрать "as UserType" и "as CityName" хотябы в tsv.offer-generator.ts
   38. await где async нужен?
+  39. проверить пересчет рейтинга от коментариев
 
 
   50. tsconfig добавил алиасы / vscode распознает пути, а копилятор нет
