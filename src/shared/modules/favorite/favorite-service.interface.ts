19813aa13e9cdf65dab20a8002c17d8dbe6bd4db
Klimov Andrey <167951608+KlimovAndreyN@users.noreply.github.com>
2024-10-30 11:11:34 +0700

    оптимизация

--- src/shared/modules/favorite/favorite-service.interface.ts
+++ src/shared/modules/favorite/favorite-service.interface.ts
@@ -5,6 +5,7 @@ export interface FavoriteService {
   find(offerId: string, userId: string): Promise<DocumentType<FavoriteEntity> | null>;
   exists(offerId: string, userId: string): Promise<boolean>;
   findByUserId(userId: string): Promise<DocumentType<FavoriteEntity>[]>;
+  findOffersByUserId(userId: string): Promise<DocumentType<FavoriteEntity>[]>;
   add(offerId: string, userId: string): Promise<DocumentType<FavoriteEntity> | null>;
   deleteById(id: string): Promise<DocumentType<FavoriteEntity> | null>;
 }
